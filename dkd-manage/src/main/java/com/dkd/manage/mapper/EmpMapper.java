83cb8bfa3a0074d13f41d1d88aa6af7fccae5df1
Smasun <3391470282@qq.com>
2026-05-10 22:03:52 +0800

    chore: stop tracking application-dev.yml

---
+++ dkd-manage/src/main/java/com/dkd/manage/mapper/EmpMapper.java
@@ -0,0 +1,73 @@
+package com.dkd.manage.mapper;
+
+import java.util.List;
+
+import com.dkd.manage.domain.Emp;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+/**
+ * 人员列表Mapper接口
+ *
+ * @author Smasun
+ * @date 2026-05-09
+ */
+@Mapper
+public interface EmpMapper {
+    /**
+     * 查询人员列表
+     *
+     * @param id 人员列表主键
+     * @return 人员列表
+     */
+    public Emp selectEmpById(Long id);
+
+    /**
+     * 查询人员列表列表
+     *
+     * @param emp 人员列表
+     * @return 人员列表集合
+     */
+    public List<Emp> selectEmpList(Emp emp);
+
+    /**
+     * 新增人员列表
+     *
+     * @param emp 人员列表
+     * @return 结果
+     */
+    public int insertEmp(Emp emp);
+
+    /**
+     * 修改人员列表
+     *
+     * @param emp 人员列表
+     * @return 结果
+     */
+    public int updateEmp(Emp emp);
+
+    /**
+     * 删除人员列表
+     *
+     * @param id 人员列表主键
+     * @return 结果
+     */
+    public int deleteEmpById(Long id);
+
+    /**
+     * 批量删除人员列表
+     *
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteEmpByIds(Long[] ids);
+
+    /**
+     * 根据区域id修改员工列表
+     *
+     * @param emp
+     */
+    @Update("update tb_emp set region_name=#{regionName} where region_id=#{regionId}")
+    void updateByRegionId(@Param("regionName") String regionName, @Param("regionId") Long regionId);
+}
